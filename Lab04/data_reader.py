ab7461d877033e8d814f7ff46433dd6c8429ebef
Elentiya <hornik.daria@gmail.com>
2021-06-11 23:26:18 +0200

    Remove not finished task

--- Lab04/data_reader.py
+++
@@ -1,9 +0,0 @@
-class File:
-
-    def __init__(self, id, label1, label2, rating, text):
-        self.id = id
-        self.label1 = label1
-        self.label2 = label2
-        self.rating = rating
-        self.text = text
-
