fc2b4bf367611646685e327a9b8bd96a13f1d54a
Elentiya <hornik.daria@gmail.com>
2021-06-11 23:23:31 +0200

    removed task

---
+++ Lab04/data_reader.py
@@ -0,0 +1,9 @@
+class File:
+
+    def __init__(self, id, label1, label2, rating, text):
+        self.id = id
+        self.label1 = label1
+        self.label2 = label2
+        self.rating = rating
+        self.text = text
+
